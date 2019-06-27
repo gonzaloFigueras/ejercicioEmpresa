b323cee46c77d017ca0e206f2b5ef5cfc62197f8
gonzalo <gonzalofigueras@gmail.com>
2019-06-27 15:27:32 -0300

    Second Commit

---
+++ src/ejerecicioJavaEmpresa/Interfaz.java
@@ -0,0 +1,16 @@
+package ejerecicioJavaEmpresa;
+
+
+public interface Interfaz {
+    
+    public void crearEmpleado();
+    
+    public void listar();
+    
+    public void guardarArray();
+    
+    public void calcularPagoTotal();
+    
+    public void calcularPagoPorCategoria(); 
+        
+}
--- src/ejerecicioJavaEmpresa/Sucursal.java
+++ src/ejerecicioJavaEmpresa/Sucursal.java
@@ -1,15 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejerecicioJavaEmpresa;
 
-/**
- *
- * @author gonzalo
- */
-public class Sucursal {
+import java.util.ArrayList;
+
+public class Sucursal implements Interfaz {
+    
+    ArrayList<Persona> personas = new ArrayList();
+
+    @Override
+    public void crearEmpleado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+       @Override
+    public void guardarArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void calcularPagoTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void calcularPagoPorCategoria() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+ 
     
     
     
